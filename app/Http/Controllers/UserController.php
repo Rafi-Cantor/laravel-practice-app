ffe642dc896d8d2d53ad2061976d66f1d1dc696c
Rafi Cantor <raficantor1@gmail.com>
2025-01-24 00:22:27 +0000

    Form validation messages and login fail message

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Session;
+
 
 class UserController extends Controller
 {
@@ -14,8 +16,19 @@ class UserController extends Controller
             'name' => ['required', 'min:3', 'max:10', Rule::unique('users', 'name')],
             'email' => ['required', 'email', Rule::unique('users', 'email')],
             'password' => ['required', 'min:8', 'max:200']
+        ], [
+            'name.required' => 'The name field is required.',
+            'name.min' => 'The name must be at least 3 characters.',
+            'name.max' => 'The name may not be greater than 10 characters.',
+            'name.unique' => 'This name is already taken.',
+            'email.required' => 'The email field is required.',
+            'email.email' => 'Please enter a valid email address.',
+            'email.unique' => 'This email is already registered.',
+            'password.required' => 'The password field is required.',
+            'password.min' => 'The password must be at least 8 characters.',
+            'password.max' => 'The password may not be greater than 200 characters.'
         ]);
-
+        
         $incomingFields['password'] = bcrypt($incomingFields['password']);
         $user = User::create($incomingFields);
         auth()->guard()->login($user);
@@ -32,15 +45,21 @@ class UserController extends Controller
         $incomingFields = $request->validate([
             'loginname' => ['required'],
             'loginpassword' => ['required']
-        ]);
+        ],
+        [
+            'loginname.required' => 'The name field is required.',
+            'loginpassword.required' => 'The password field is required.',
+        ] 
+    );
 
-        if (auth()->attempt(['name' => $incomingFields['loginname'], 'password' => $incomingFields['loginpassword']])) {
-            $request->session()->regenerate();
-        }
+    if (auth()->attempt(['name' => $incomingFields['loginname'], 'password' => $incomingFields['loginpassword']])) {
+        $request->session()->regenerate();
         return redirect('/');
+    } else {
+        return redirect()->back()->with('error', 'Login failed. Please check name and password. ');
     }
 
-
+    }
 
     public function getLogin(Request $request) {
         return view('/login');
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,18 +11,38 @@
         <form action="/register" method="POST">
             @csrf
             <input name="name" type="text" placeholder="name">
+            @error('name')
+                <div class="">{{ $message }}</div>
+            @enderror
             <input name="email" type="text" placeholder="email">
+            @error('email')
+                <div class="">{{ $message }}</div>
+            @enderror
             <input name="password" type="password" placeholder="password">
+            @error('password')
+                <div class="">{{ $message }}</div>
+            @enderror
             <button>Register</button>
         </form>
     </div>
     <div style="border: 3px solid black;">
+        
+
         <h2>Login</h2>
         <form action="/login" method="POST">
             @csrf
             <input name="loginname" type="text" placeholder="name">
+            @error('loginname')
+                <div class="">{{ $message }}</div>
+            @enderror
             <input name="loginpassword" type="password" placeholder="password">
+            @error('loginpassword')
+                <div class="">{{ $message }}</div>
+            @enderror
             <button>Log in</button>
+            @if (Session::has('error'))
+                <div class="">{{ Session::get('error') }}</div>
+            @endif
         </form>
     </div>
 </body>
